dfea08e70c3c4c1b8e4d37839a299432742dd84b
manumanoj5415 <153482424+manumanoj5415@users.noreply.github.com>
2024-04-12 22:34:12 +0530

    First Commit

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"//react-weather-app-new-api-main",
+ 
   plugins: [react()],
   server: {
     port: 3000,
